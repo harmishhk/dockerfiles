80fd6065a179619aa28828ea241b8ddccb366bbd
harmish khambhaita <harmish@hotmail.com>
2015-07-14 17:57:18 +0200

    updating morse dockerfiles
    - making default dockerfile
    - adding morse_ros in the ros-pr2 dockerfile
    - updating readme

---
+++ docker/morse/morse/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# return if cannot execute
+set -e
+
+# execute given morse command
+if [[ "$HEADLESS" == true ]]
+then
+    echo "using xvfb screen"
+    export DISPLAY=:1
+    Xvfb -screen 0 1024x768x24 :1 >/dev/null 2>&1 & morse "$@"
+else
+    morse "$@"
+fi
