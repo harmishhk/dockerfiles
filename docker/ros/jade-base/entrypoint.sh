c3d7c0ed4cc1de5dab0527e75b10c694bc00bcda
harmish khambhaita <harmish@laas.fr>
2015-08-27 12:19:52 +0200

    updating ros dockerfiles with optimized version from devel branch

--- docker/ros/jade-base/entrypoint.sh
+++ docker/ros/jade-base/entrypoint.sh
@@ -1,10 +1,17 @@
-#!/usr/bin/zsh
+#!/bin/bash
 
 # return if cannot execute
 set -e
 
 # setup ros
-source /opt/ros/$ROS_DISTRO/setup.zsh
+source /opt/ros/$ROS_DISTRO/setup.bash
+
+# setup master when asked
+if [[ $1 == "-m" ]]
+then
+    export ROS_MASTER_URI=http://$2:11311
+    shift 2
+fi
 
 # executre given command
 exec "$@"
