011349904e6e79583f6b3e390debe1d12f15b2ac
harmish khambhaita <harmish@laas.fr>
2015-08-31 22:48:01 +0200

    updating docker-ros related images - removing -m option - making ssh work with non-root user

--- docker/ros/jade-base/entrypoint.sh
+++ docker/ros/jade-base/entrypoint.sh
@@ -6,12 +6,5 @@ set -e
 # setup ros
 source /opt/ros/$ROS_DISTRO/setup.bash ""
 
-# setup master when asked
-if [[ $1 == "-m" ]]
-then
-    export ROS_MASTER_URI=http://$2:11311
-    shift 2
-fi
-
 # executre given command
 exec "$@"
