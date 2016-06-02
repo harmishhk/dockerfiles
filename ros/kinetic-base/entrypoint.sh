15665b0ce468ce09dd740af48771b0e55c40d9bd
harmish khambhaita <harmish@laas.fr>
2016-06-02 17:47:57 +0200

    adding ros-kinetic images

---
+++ ros/kinetic-base/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# return if cannot execute
+set -e
+
+# setup ros
+source /opt/ros/$ROS_DISTRO/setup.bash ""
+export CMAKE_INCLUDE_PATH=/opt/local/catch/include:$CMAKE_INCLUDE_PATH
+
+# executre given command
+exec "$@"
