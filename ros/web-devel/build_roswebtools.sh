8339fdd6521c651043bd9bb9ef4845c8e87613f1
harmish khambhaita <harmish@laas.fr>
2016-07-26 14:38:12 +0200

    building only available roswebtools

--- ros/web-devel/build_roswebtools.sh
+++ ros/web-devel/build_roswebtools.sh
@@ -3,26 +3,39 @@
 # return if cannot execute
 set -e
 
+ROSLIB_DIR=/roswebtools/roslibjs
+ROS2D_DIR=/roswebtools/ros2djs
+ROS3D_DIR=/roswebtools/ros3djs/utils
+NAV2D_DIR=/roswebtools/nav2djs/utils
+
 # build roslibjs
-echo "building roslibjs\n"
-cd /roswebtools/roslibjs/ && \
-npm install . && \
-grunt build --force
+if [ -d "$ROSLIB_DIR" ]; then
+    echo "building roslibjs\n"
+    cd $ROSLIB_DIR && \
+    npm install . && \
+    grunt build --force
+fi
 
 # build ros2djs
-echo "building ros2djs\n"
-cd /roswebtools/ros2djs/ && \
-npm install . && \
-grunt build --force
+if [ -d "$ROS2D_DIR" ]; then
+    echo "building ros2djs\n"
+    cd $ROS2D_DIR && \
+    npm install . && \
+    grunt build --force
+fi
 
 # build ros3djs
-echo "building ros3djs\n"
-cd /roswebtools/ros3djs/utils/ && \
-npm install . && \
-grunt build --force
+if [ -d "$ROS3D_DIR" ]; then
+    echo "building ros3djs\n"
+    cd $ROS3D_DIR && \
+    npm install . && \
+    grunt build --force
+fi
 
 # build nav2djs
-echo "building nav2djs\n"
-cd /roswebtools/nav2djs/utils/ && \
-npm install . && \
-grunt build --force
+if [ -d "$NAV2D_DIR" ]; then
+    echo "building nav2djs\n"
+    cd $NAV2D_DIR && \
+    npm install . && \
+    grunt build --force
+fi
