80be6dea113e2881017e5bf67632f42eddaae03f
harmish khambhaita <harmish@laas.fr>
2016-08-23 17:38:19 +0200

    fixing entrypoint in ros images

--- ros/indigo-devel/entrypoint.sh
+++ ros/indigo-devel/entrypoint.sh
@@ -4,7 +4,7 @@
 set -e
 
 # setup ros
-source "/opt/ros/$ROS_DISTRO/setup.bash"
+source "/opt/ros/$ROS_DISTRO/setup.bash" ""
 export CMAKE_INCLUDE_PATH="/opt/local/catch/include:$CMAKE_INCLUDE_PATH"
 
 # executre given command
