3b0fe02fd045eca8a0b67b4f39ce64fe5a11d036
harmish khambhaita <harmish@laas.fr>
2016-12-01 17:57:52 +0100

    exporting bash-functions for ros-based-images

--- ros/kinetic-devel/entrypoint.sh
+++ ros/kinetic-devel/entrypoint.sh
@@ -5,6 +5,7 @@ set -e
 
 # setup ros
 source "/opt/ros/$ROS_DISTRO/setup.bash" ""
+for func in $(declare -F | awk '{print $3}'); do export -f $func; done
 export CMAKE_INCLUDE_PATH="/opt/local/catch/include:$CMAKE_INCLUDE_PATH"
 
 # executre given command
